b8b71e70c0a982149af51f95005a30b6edad3b2e
ghilardi.19320 <ghilardi.19320@LABI1-6PC12>
2023-03-27 08:35:18 +0200

    Task1

---
+++ GhilN_Prova_Infotest/GhilN_Prova_Candidato.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhilN_Prova_Infotest
+{
+    public abstract class GhilN_Prova_Candidato
+    {
+        int GhilN_Prova_Matricola;
+        string GhilN_Prova_nome;
+
+        public int Matricola { get; set; }
+
+        public string Nome { get; set; }
+
+        public GhilN_Prova_Candidato()
+        {
+            Nome = "";
+            Matricola = 0;
+        }
+        public GhilN_Prova_Candidato(int matricola)
+        {
+            Matricola = matricola;
+            Nome = "";
+        }
+        public GhilN_Prova_Candidato(string nome)
+        {
+            Nome = nome;
+            Matricola=0;
+        }
+        public GhilN_Prova_Candidato(string nome, int matricola)
+        {
+            Nome = nome;
+            Matricola = matricola;
+        }
+
+        public abstract bool isIdoneo();
+
+        public abstract int punteggio();
+
+
+    }
+
+
+}
