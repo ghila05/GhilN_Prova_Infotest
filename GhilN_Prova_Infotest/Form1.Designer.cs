5224bb43e7970744d35c2bf6dfb48bb4d80e9d0f
ghilardi.19320 <ghilardi.19320@LABI1-6PC12>
2023-03-27 10:58:57 +0200

    Task5

--- GhilN_Prova_Infotest/Form1.Designer.cs
+++ GhilN_Prova_Infotest/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(399, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(389, 407);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +54,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
--- GhilN_Prova_Infotest/Form1.cs
+++ GhilN_Prova_Infotest/Form1.cs
@@ -16,12 +16,80 @@ namespace GhilN_Prova_Infotest
         {
             InitializeComponent();
         }
+        List<GhilN_Prova_Candidato> lista;
+
+        bool first = true;
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            lista = new List<GhilN_Prova_Candidato>();
+
+
+            if (first)
+            {
+                listView1.View = View.Details;
+                listView1.FullRowSelect = true;
+                first = false;
+
+                listView1.Columns.Add("CANDIDATO", 60);
+
+
+                ListViewItem item = new ListViewItem();
 
 
 
+            }
+            
+
+        }
+        public void AddCandidato(GhilN_Prova_Candidato u)
+        {
+            if(u == null)
+            {
+                throw new Exception("invalid");
+            }
+            lista.Add(u);
+
+        }
+        public string[] Candidati()
+        {
+            string[] cand = new string[] {};
+     
+            for(int i=0; i<lista.Count(); i++)
+            {
+                cand[i] = lista[i].Nome;
+            }
+            return cand;
+        }
+
+        public void Mod(GhilN_Prova_Candidato u)
+        {
+            if (u == null) throw new Exception("invalid argument");
+            for(int i=0; i<lista.Count(); i++)
+            {
+                if (lista[i].Matricola == u.Matricola)
+                {
+                    lista[i].Nome = u.Nome;
+                    
+                }
+            }
+
+        }
+        public void Delete(GhilN_Prova_Candidato d)
+        {
+            if (d == null) throw new Exception("invalid argument");
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                if (lista[i].Matricola == d.Matricola)
+                {
+                    lista.RemoveAt(i);
+                }
+            }
+        }
+
+        public void Ordina()
+        {
+            lista.Sort();
 
         }
     }
